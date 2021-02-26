4419fa7a8be968b0ace09fb5b423914f8a2cbfb6
devbnj <devb@linux.com>
2021-02-26 16:01:14 -0500

    code change

--- routes/01logfetchResource.js
+++ routes/01logfetchResource.js
@@ -8,27 +8,27 @@ let AWS = require('aws-sdk');
 let fs = require("fs");
 let fs1 = require("fs");
 AWS.config.update({region: "us-east-1"});
-var s3 = new AWS.S3();
+var bctg = new AWS.S3();
 
 /**
  * function: getS3
  * @param {*} sobject 
  */
-async function getS3(sobject) {
+async function getfirehose(sobject) {
     return new Promise(
         (resolve, reject) => {
             let paramsData = JSON.parse(fs.readFileSync('logobjects.json', 'utf8'));
             for (var i = 0; i <paramsData.length; i++) {
-                let newParams = paramsData[i];
-                s3.getObject(newParams, function (oerr, odata) {
+                let plop = paramsData[i];
+                bctg.getObject(plop, function (oerr, odata) {
                     if (oerr) console.log(oerr, oerr.stack);
                     else {
-                        var fullPath = newParams.Key; 
-                        var newFilename = fullPath.replace(/^.*[\\\/]/, '')
-                        console.log(newFilename);
-                        fs1.writeFileSync('files/'+newFilename, odata.Body, (nerr) => {
+                        var frp = plop.Key; 
+                        var nfp = frp.replace(/^.*[\\\/]/, '')
+                        console.log(nfp);
+                        fs1.writeFileSync('files/'+nfp, odata.Body, (nerr) => {
                             if (nerr) throw nerr;
-                            console.log(newFilename, ' file has been saved!');
+                            console.log(nfp, ' file has been saved!');
                         });
                     }
                 })
@@ -37,6 +37,6 @@ async function getS3(sobject) {
 }
 
 exports.logfetchResource = function (bparams) {
-    getS3()
+    getfirehose()
     .then ( (result) => console.log('Result', result))
 }
